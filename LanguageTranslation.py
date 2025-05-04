69ed0f4cb10a7916feed0c76bc1ef280f928dc0f
priyansusikdar2 <priyansusikdar36@gmail.com>
2025-05-04 22:01:30 +0530

    Update LanguageTranslation.py

--- LanguageTranslation.py
+++ LanguageTranslation.py
@@ -1,35 +1,40 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
-# Sample parallel corpus
+# Sample parallel corpus (English → French)
 english_sentences = ["hello", "how are you", "thank you", "good night"]
 french_sentences = ["bonjour", "comment ça va", "merci", "bonne nuit"]
 
-# Tokenize source (English)
+# Basic preprocessing (lowercase and whitespace tokenization)
+def basic_tokenize(sentences):
+    return [' '.join(s.lower().split()) for s in sentences]
+
+english_sentences = basic_tokenize(english_sentences)
+french_sentences = ["<start> " + s.lower() + " <end>" for s in french_sentences]
+
+# Tokenization
 src_tokenizer = tf.keras.preprocessing.text.Tokenizer(filters='')
 src_tokenizer.fit_on_texts(english_sentences)
 src_sequences = src_tokenizer.texts_to_sequences(english_sentences)
-src_word_index = src_tokenizer.word_index
-src_vocab_size = len(src_word_index) + 1
+src_vocab_size = len(src_tokenizer.word_index) + 1
 
-# Tokenize target (French) with <start> and <end>
-french_sentences = [f"<start> {s} <end>" for s in french_sentences]
 tgt_tokenizer = tf.keras.preprocessing.text.Tokenizer(filters='')
 tgt_tokenizer.fit_on_texts(french_sentences)
 tgt_sequences = tgt_tokenizer.texts_to_sequences(french_sentences)
 tgt_word_index = tgt_tokenizer.word_index
 tgt_index_word = {i: w for w, i in tgt_word_index.items()}
-tgt_vocab_size = len(tgt_word_index) + 1
+tgt_vocab_size = len(tgt_tokenizer.word_index) + 1
 
 # Padding
 src_padded = tf.keras.preprocessing.sequence.pad_sequences(src_sequences, padding='post')
 tgt_padded = tf.keras.preprocessing.sequence.pad_sequences(tgt_sequences, padding='post')
 
-# Split decoder input and target
+# Prepare decoder input and target
 decoder_input = tgt_padded[:, :-1]
 decoder_target = tf.keras.utils.to_categorical(tgt_padded[:, 1:], num_classes=tgt_vocab_size)
 
-# Build Seq2Seq model
+# Model parameters
 embedding_dim = 64
 latent_dim = 64
 
@@ -48,30 +53,40 @@ decoder_outputs, _, _ = decoder_lstm(dec_emb, initial_state=encoder_states)
 decoder_dense = tf.keras.layers.Dense(tgt_vocab_size, activation='softmax')
 decoder_outputs = decoder_dense(decoder_outputs)
 
-# Compile model
+# Compile and train
 model = tf.keras.Model([encoder_inputs, decoder_inputs], decoder_outputs)
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit([src_padded, decoder_input], decoder_target, epochs=300, verbose=0)
+history = model.fit([src_padded, decoder_input], decoder_target, epochs=300, verbose=0)
+
+# Plot accuracy
+plt.plot(history.history['accuracy'])
+plt.title("Model Accuracy")
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+plt.grid(True)
+plt.show()
 
 # Inference models
-# Encoder inference
 encoder_model = tf.keras.Model(encoder_inputs, encoder_states)
 
-# Decoder inference
 decoder_state_input_h = tf.keras.Input(shape=(latent_dim,))
 decoder_state_input_c = tf.keras.Input(shape=(latent_dim,))
 decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
-dec_emb2 = dec_emb_layer(decoder_inputs)
+
+decoder_inputs_inf = tf.keras.Input(shape=(1,))
+dec_emb2 = dec_emb_layer(decoder_inputs_inf)
 decoder_outputs2, state_h2, state_c2 = decoder_lstm(dec_emb2, initial_state=decoder_states_inputs)
 decoder_states2 = [state_h2, state_c2]
 decoder_outputs2 = decoder_dense(decoder_outputs2)
+
 decoder_model = tf.keras.Model(
-    [decoder_inputs] + decoder_states_inputs,
+    [decoder_inputs_inf] + decoder_states_inputs,
     [decoder_outputs2] + decoder_states2
 )
 
-# Translation function
+# Translate function (no NLP)
 def translate(input_text):
+    input_text = ' '.join(input_text.lower().split())
     seq = src_tokenizer.texts_to_sequences([input_text])
     seq = tf.keras.preprocessing.sequence.pad_sequences(seq, maxlen=src_padded.shape[1], padding='post')
     states_value = encoder_model.predict(seq)
